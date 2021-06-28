090887651d8d32becfa661823baab24534a7bb7b
Carlos Eduardo <78118717+eduardo92005-debug@users.noreply.github.com>
2021-06-28 05:36:49 -0300

    Update warrior.py

--- Strategy Pattern/src/warrior.py
+++ Strategy Pattern/src/warrior.py
@@ -1,10 +1,11 @@
 from typing import Type
 from .hability import IHability
 
-
+#Bib Type muito importante para explicitar tipagem de dados
 class Warrior:
    def __init__(self, hability: Type[IHability]):
       self.hability = hability
+   #Implementa comportamento e atributos atraves da interface IHability
    def action(self):
       self.hability.comportamental()
    def attributes(self):
